4e01c34641018cba3dae0bc1f7a487127a8e7f54
Rob McQueen <robmcqueen@pivotal-guest-108.sf.pivotallabs.com>
2013-06-29 00:52:18 -0700

    woooo

---
+++ omnom/main/constants.py
@@ -0,0 +1,4 @@
+EXPIRY_CHOICES = [
+    (1,"1 hour"),
+    (1,"2 hours"),
+    (1,"3 hours"),
---
+++ omnom/main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class PickupRequest(models.Model):
+    description = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    requester = models.ForeignKey(User,related_name="requests")
+    
+    is_fulfilled = models.BooleanField(default=False)
+    fulfilled_by = models.ForeignKey(User,related_name="fulfilled_requests")
+    is_picked_up = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    expiry = models.IntegerField(default=24) # number of hours since created
+
+class Feedback(models.Model):
+    pickup_request = models.ForeignKey(PickupRequest)
+    from_user = models.ForeignKey(User,related_name="feedbacks_given")
+    to_user = models.ForeignKey(User,related_name="feedbacks_received")
+
+    rating = models.IntegerField()
+    feedback = models.TextField()
+    
+    
