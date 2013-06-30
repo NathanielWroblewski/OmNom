b385dbfb73d3f8f296dedea1a422fd847c6a36b5
sidney zhang <sidazhang89@gmail.com>
2013-06-29 18:35:06 -0700

    Message confirmation finalised

--- omnom/main/views.py
+++ omnom/main/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from main.models import *
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-
+from django.core.urlresolvers import reverse
 from django.contrib.auth import logout as auth_logout
 
 @login_required
@@ -17,7 +17,8 @@ def logout(request):
 @login_required
 def create_pickup_request(request):
     if request.method != "POST":
-        raise Http404
+        all_requests = PickupRequest.objects.all()
+        return render_to_response("create_request.html", {"all_requests": all_requests},RequestContext(request))
 
     new_request = PickupRequest(
         description = request.POST.get("description",""),
@@ -26,41 +27,45 @@ def create_pickup_request(request):
         requester=request.user
     )
     new_request.save()
-    return HttpResponse("OK")
+    return redirect("create_pickup_request")
 
 @login_required
 def actions_fulfill_request(request,request_id):
-    # try
-    #     pickup_request = PickupRequest.objects.get(id=request_id)
-    #     donnor_phone_number = pickup_request.requester.user_profile.phone_number
-    #     volunteer = request.user
-    #     pickup_request.fulfilled_by = volunteer
-    #     client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    #     message = client.sms.messages.create(to=phone_number, from_="+17202599396", body="Someone has confirmed that they want to pick up your food! %s" % volunteer.id + "-" pickup_request.id)
-    # except PickupRequest.DoesNotExist:
-    #     return HttpResponse("Pickup request does not exist %s" % request_id)
+    try:
+        pickup_request = PickupRequest.objects.get(id=request_id)
+        donnor_phone_number = UserProfile.objects.get(id=pickup_request.requester.id).phone_number
+        volunteer = request.user
+        client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        message = client.sms.messages.create(to=donnor_phone_number, from_="+17202599396", body="Someone has confirmed that they want to pick up your food! Please reply with %s-%s" % (volunteer.id,pickup_request.id))
+    except PickupRequest.DoesNotExist:
+        return HttpResponse("Pickup request does not exist %s" % request_id)
 
-    return HttpResponse("Fulfill Request Endpoint")
+    return redirect(reverse('direction_map'))
 
 @login_required
 def actions_confirm_request(request,request_id):
     return HttpResponse("Confirm Request Endpoint")
 
-@login_required
 def twilio_callback(request):
-    # from_ = request.GET.get("From")
-    # body = request.GET.get("Body")
-    # body = body.split("-")
-    # try
-    #     request_user_profile = UserProfile.objects.get(phone_number=from_)
-    #     pickup_request = PickupRequest.objects.get(id=body[0])
-    #     volunteer = User.objects.get(id=body[1])
-    #     pickup_request.is_fulfilled = True
-    #     pickup_request.save()
-    # except PickupRequest.DoesNotExist:
-    #     return HttpResponse("Pickup request does not exist %s" % body)
-    # except UserProfile.DoesNotExist:
-    #     return HttpResponse("User profile does not exist %s" % from_)
+    from_ = request.GET.get("From")
+    body = request.GET.get("Body")
+    body = body.split("-")
+    print body
+    try:
+        request_user_profile = UserProfile.objects.get(phone_number=from_)
+        volunteer = User.objects.get(id=body[0])
+        pickup_request = PickupRequest.objects.get(id=body[1])
+        pickup_request.is_fulfilled = True
+        pickup_request.fulfilled_by = volunteer
+        pickup_request.save()
+        volunteer_phone_number = UserProfile.objects.get(user_id=volunteer.id).phone_number
+
+        client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        message = client.sms.messages.create(to=volunteer_phone_number, from_="+17202599396", body="Donor has already confirmed that you can pick up the food! Come and Grab it, bro!")
+    except PickupRequest.DoesNotExist:
+        return HttpResponse("Pickup request does not exist %s" % body)
+    except UserProfile.DoesNotExist:
+        return HttpResponse("User profile does not exist %s" % from_)
     return HttpResponse("Twilio Callback")
 
 @login_required
@@ -82,13 +87,14 @@ def create_profile(request):
         user_profile.save()
         return redirect("/")
     else:
-        """HACK FIXME"""
-        user_profile = UserProfile.objects.get(user_id=request.user.id);
+        # """HACK FIXME"""
+        # user_profile = UserProfile.objects.get(user_id=request.user.id);
         return render_to_response("create_profile.html",{'user_profile':user_profile},RequestContext(request))
 
 @login_required
 def donation_map(request):
-	return render_to_response("donation_map.html",RequestContext(request))
+    all_requests = PickupRequest.objects.all()
+    return render_to_response("donation_map.html", {"all_requests": all_requests},RequestContext(request))
 
 @login_required
 def requests(request):
@@ -100,8 +106,8 @@ def get_pic_url(request):
     return render_to_response("person.html", {'picurl':picurl})
 
 @login_required
-def msg_confirmation(request):
-	return render_to_response("msg_confirmation.html",RequestContext(request))
+def msg_confirmation(request, request_id):
+	return render_to_response("msg_confirmation.html", {'request_id': request_id},RequestContext(request))
 
 @login_required
 def direction_map(request):
